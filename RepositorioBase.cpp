4ab42670d484fcf7e17757e3803c9660bdaba8e5
NicolasSalvador13 <nicolas.salvador.br@gmail.com>
2025-11-15 19:20:01 -0300

    Atualizando RepositorioBase.cpp

---
+++ RepositorioBase.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "RepositorioBase.h"
+
+std::vector <std::string> RepositorioBase::LerLinhasDoArquivo() {
+
+}
+
+void RepositorioBase::escreverLinhasNoArquivo(std::vector <std::string>) {
+    
+}
