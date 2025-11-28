b7d7237b8e3fe22e48ab02f614fa66214e595611
muriloSousa05 <murilo.sousa@ufv.br>
2025-11-27 21:59:02 -0300

    Update ControladorEstudo.h

--- ControladorEstudo.h
+++ ControladorEstudo.h
@@ -1,13 +1,28 @@
 #ifndef CONTROLADOR_ESTUDO_H
 #define CONTROLADOR_ESTUDO_H
+
 #include "usuario.h"
 #include "SessaoEstudo.h"
+#include "ControladorGamificacao.h"
+#include <string>
 
 class ControladorEstudo {
+private:
+    SessaoEstudo* sessaoAtiva;
+    std::string estado; // parado, rodando, pausado
+    ControladorGamificacao* controladorGami;
+
 public:
-    void salvarSessao(Usuario* u, SessaoEstudo& sessao) {
-        sessao.armazenar();
-        u->getHistorico().adicionarSessao(sessao);
-    }
+    ControladorEstudo(ControladorGamificacao* controladorG);
+    ~ControladorEstudo();
+
+    void iniciarSessao(int duracao);
+    void pausarSessao();
+    void finalizarSessao();
+    void salvarSessao(Usuario* u, SessaoEstudo& sessao);
+    
+    SessaoEstudo* getSessaoAtiva();
+    std::string getEstado() const;
 };
+
 #endif
