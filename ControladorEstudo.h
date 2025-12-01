0fd6e3d2e517e9e94089f89110a45c6710bc4703
GabrielCarneiro89 <gabriel.assumpcao@ufv.br>
2025-12-01 18:11:12 -0300

    coments

--- ControladorEstudo.h
+++ ControladorEstudo.h
@@ -1,21 +1,17 @@
 #ifndef CONTROLADOR_ESTUDO_H
 #define CONTROLADOR_ESTUDO_H
 
-// --- Dependências de Associação (Ponteiros recebidos da Main) ---
 #include "Usuario.h"
 #include "RepositorioEstudos.h"
 #include "RepositorioGamificacao.h"
 
-// --- Dependência de Composição (A Tela pertence ao Controlador) ---
 #include "TelaEstudo.h"
-
-// --- Dependência de Uso (O objeto que vamos manipular) ---
 #include "SessaoEstudo.h"
 
 class ControladorEstudo {
 private:
     // Poteiros para os objetos principais do sistema (Injeção de Dependência)
-    // O Controlador USA estes objetos, mas não é dono deles (não dá delete)
+    // O Controlador usa estes objetos, mas não é dono deles (não dá delete)
     Usuario* usuario;
     RepositorioEstudos* repoEstudos;
     RepositorioGamificacao* repoGamificacao; 
@@ -29,18 +25,15 @@ private:
     SessaoEstudo sessaoAtual;
 
 public:
-    // Construtor
-    // Recebe os ponteiros fundamentais inicializados na main
-    ControladorEstudo(Usuario* usuario, 
-                      RepositorioEstudos* repoEstudos, 
-                      RepositorioGamificacao* repoGamificacao);
+    
+    ControladorEstudo(Usuario* usuario, RepositorioEstudos* repoEstudos, RepositorioGamificacao* repoGamificacao);
 
     // Método Principal
     // Inicia o loop do menu de estudos (Chamado pelo ControladorPrincipal)
     void executar();
 
 private:
-    // --- Métodos Auxiliares (Lógica Interna) ---
+    // --- Métodos Auxiliares  ---
 
     // 1. Pede disciplina/descrição e prepara o objeto sessaoAtual
     void iniciarNovaSessao();
